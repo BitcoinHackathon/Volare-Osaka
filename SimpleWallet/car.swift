520c7ae707e93e41e21fb782f5a45795a54594bd
鶴見侑樹 <tsurumi0302@gmail.com>
2018-08-19 00:42:08 +0900

    create car screen

---
+++ SimpleWallet/car.swift
@@ -0,0 +1,13 @@
+//
+//  car.swift
+//  SimpleWallet
+//
+//  Created by 鶴見侑樹 on 2018/08/19.
+//  Copyright © 2018年 Akifumi Fujita. All rights reserved.
+//
+
+import UIKit
+
+class car: NSObject {
+
+}
